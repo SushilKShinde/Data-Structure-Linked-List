05425e255e82f37e956a096bff7df663ef94984c
Sushil <skshinde2028@gmail.com>
2023-05-04 14:33:24 +0530

    [Sushil] creating linked list

--- Data Structure-Linked List/Program.cs
+++ Data Structure-Linked List/Program.cs
@@ -11,7 +11,28 @@ namespace Data_Structure_Linked_List
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data structure Linked list");
+            Console.WriteLine("Please select option from below\n");
+            Console.WriteLine("1.BuiltIn linked list\n2. ");
+            int option = Convert.ToInt32(Console.ReadLine());
 
+            switch(option)
+            {
+                case 1:
+                    LinkedList<int> list = new LinkedList<int>();
+                    list.AddLast(10);
+                    list.AddLast(20);
+                    list.AddLast(30);
+                    //to print linked list
+                    Console.WriteLine("List items are");
+                    foreach (int i in list)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    break;
+            }
         }
     }
 }
